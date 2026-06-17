eafb5973bbec7ca9168b3eb9b45a6e5bb5fec51d
FedeAMasin <FefeS1836@gmail.com>
2026-06-17 20:51:55 -0300

    Arreglo de config en vite.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react';
 
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 
 export default defineConfig({
  plugins: [react()],
